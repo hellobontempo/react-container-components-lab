5b9ca08b855f87f504d19a2d61d9897325d505de
claire <clairebontempo@gmail.com>
2021-03-04 11:31:02 -0800

    commit tree a15ce5aa227f7dbce49b6670217326bcc5122a7f

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+export default class LatestMovieReviewsContainer extends React.Component {
+
+    state = {
+        reviews: []
+    }
+
+    componentDidMount(){
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=${process.env.REACT_APP_API_KEY}`)
+        .then( r => r.json())
+            .then( data => this.setState({reviews: data.results}))
+
+    }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                < MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,41 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+// const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+// const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+//             + `api-key=${process.env.REACT_APP_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+    
+    // componentDidMount(){
+    //     fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${process.env.REACT_APP_API_KEY}&query=${this.state.searchTerm}`)
+    //         .then(resp => resp.json())
+    //             .then(data => this.setState({reviews: data.results}))
+    // }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${process.env.REACT_APP_API_KEY}&query=${this.state.searchTerm}`)
+        .then(resp => resp.json())
+            .then(data => this.setState({reviews: data.results}))
+    }
+
+    render () {
+        console.log(this.state)
+        return (
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={(e)=> this.setState({searchTerm: e.target.value})} type="text"></input>
+                </form>
+                <div><MovieReviews reviews={this.state.reviews} /> </div>
+            </div>
+        )
+    }
+}
